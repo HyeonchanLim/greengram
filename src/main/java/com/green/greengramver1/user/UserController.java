d58fe8c62b8fc3dc962e53f2ecab4cdde1aff6eb
LimHyeonChan <lhc0240@gmail.com>
2024-11-19 17:50:09 +0900

    aa

---
+++ src/main/java/com/green/greengramver1/user/UserController.java
@@ -0,0 +1,27 @@
+package com.green.greengramver1.user;
+
+import com.green.greengramver1.common.model.ResultResponse;
+import com.green.greengramver1.user.model.UserInsReq;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("user")
+public class UserController {
+    private final UserService service;
+
+    @PostMapping("sign-up")
+    @Operation(summary = "회원 가입")
+    public ResultResponse<Integer> signUp(@RequestBody UserInsReq p){
+        int result = 0;
+        return ResultResponse.<Integer>builder().resultMessage("회원가입 완료").resultData(result).build();
+    }
+
+}
